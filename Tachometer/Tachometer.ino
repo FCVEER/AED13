52cbfa44c066cceba6e090c0e52308418f799d46
FCVEER <rik@veerhoek.nl>
2024-10-03 14:33:20 +0200

    Signed-off-by: FCVEER <rik@veerhoek.nl>

--- Tachometer/Tachometer.ino
+++ Tachometer/Tachometer.ino
@@ -1,42 +1,48 @@
+//pin nummers toekennen
 const int sensorPin = 2;
 const int potPin = A0;
 const int pwmPin = 3;
-const int inputMotor1 = 8;
-const int inputMotor2 = 7;
 
+// Variabelen declareren
 int sensState;
 int sensPrevState;
 int sensStep;
 int sensPrevStep;
 int sensStepDif;
 int rpm;
-
-
 int pwm;
 
 void setup() {
+  // serial verbinding starten
   Serial.begin(9600);
+  // pinmodes instellen
   pinMode(sensorPin, INPUT);
   pinMode(potPin, INPUT);
   pinMode(pwmPin, OUTPUT);
 }
 
 void loop() {
+  // potentiometer uitlezen en schalen voor gebruik als pwm signaal
   pwm = map(analogRead(potPin), 1, 1023, 1, 255);
+  // digitaal signaal van de infra rood sensor uitlezen
   sensState = digitalRead(sensorPin);
 
-  digitalWrite(inputMotor1, LOW);
-  digitalWrite(inputMotor2, HIGH);
+  //motor aansturen met pwm signaal
   analogWrite(pwmPin, pwm);
 
+  //kijken of de sensorwaarde omhoog is gegaan t.o.v. de vorige meting
   if ((sensState == 1) && (sensPrevState == 0)) {
+    //tijdstip van puls opslaan en aftrekken van vorig tijdstip zodat je een tijd tussen de pulsen krijgt in ms
     sensPrevStep = sensStep;
     sensStep = millis();
     sensStepDif = sensStep - sensPrevStep;
+    // tijd tussen pulsen omrekenen naar toeren per minuut
     rpm = round((1000 / sensStepDif) * 60);
+    // data naar de serial monitor/plotter schijven
     Serial.print(pwm);
     Serial.print(" , ");
     Serial.println(rpm);
   }
+  // vorige status van IR sensor overschijven met huidige status
   sensPrevState = sensState;
 }
