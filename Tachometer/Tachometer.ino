4a4e6a7be1d2dcc1bc5e391588518c6f1063d0d7
FCVEER <rik@veerhoek.nl>
2024-10-03 11:33:23 +0200

    Signed-off-by: FCVEER <rik@veerhoek.nl>

--- Tachometer/Tachometer.ino
+++ Tachometer/Tachometer.ino
@@ -4,9 +4,6 @@ const int pwmPin = 3;
 const int inputMotor1 = 8;
 const int inputMotor2 = 7;
 
-int intervalDelay = 1000;
-int intervalCouter;
-
 int sensState;
 int sensPrevState;
 int sensStep;
@@ -18,37 +15,28 @@ int rpm;
 int pwm;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(sensorPin, INPUT);
   pinMode(potPin, INPUT);
   pinMode(pwmPin, OUTPUT);
-
 }
 
 void loop() {
-  pwm = analogRead(potPin);
+  pwm = map(analogRead(potPin), 1, 1023, 1, 255);
   sensState = digitalRead(sensorPin);
 
-  //Serial.println(pwm) ;
   digitalWrite(inputMotor1, LOW);
   digitalWrite(inputMotor2, HIGH);
   analogWrite(pwmPin, pwm);
 
-  // if (sensState == 1){
-  //   Serial.println("High");
-  // }
-  // else{
-  //   Serial.println("Low");
-  // }
-  
-  if( (sensState==1) && (sensPrevState == 0)){
+  if ((sensState == 1) && (sensPrevState == 0)) {
     sensPrevStep = sensStep;
     sensStep = millis();
     sensStepDif = sensStep - sensPrevStep;
-    rpm = round((1000/sensStepDif)*60);
+    rpm = round((1000 / sensStepDif) * 60);
+    Serial.print(pwm);
+    Serial.print(" , ");
     Serial.println(rpm);
   }
   sensPrevState = sensState;
-
 }
